5a0d7e263df668ec7cc374fec27a1e5de63746db
Djent- <inflames2361@aim.com>
2014-06-12 21:48:06 -0400

    added self.phase to square:new. love.graphics.draw() draws the canvas of each square to the scale of math.sin(self.phase)

--- main.lua
+++ main.lua
@@ -10,6 +10,7 @@ function love.update(dt)
 	if #squares == 0 then return end
 	for x = 1, #squares do
 		squares[x].angle = squares[x].angle + math.pi * dt
+		squares[x].phase = squares[x].phase + math.pi * dt
 	end
 end
 
@@ -17,7 +18,8 @@ function love.draw()
 	if #squares == 0 then return end
 	for x = 1, #squares do
 		love.graphics.draw(squares[x].canvas, squares[x].x, squares[x].y,
-			squares[x].angle, 1, 1, squares[x].w/2, squares[x].h/2)
+			squares[x].angle, 1 + math.sin(squares[x].phase),
+			1 + math.sin(squares[x].phase), squares[x].w/2, squares[x].h/2)
 	end
 end
 
@@ -32,6 +34,7 @@ function square:new(x,y)
 	self.w = 40
 	self.h = 40
 	self.angle = 0
+	self.phase = 0
 	self.canvas = love.graphics.newCanvas(40,40)
 	love.graphics.setCanvas(self.canvas)
 	love.graphics.rectangle("line", 0, 0, 40, 40)
