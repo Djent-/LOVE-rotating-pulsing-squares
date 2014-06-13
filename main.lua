54d45678b34f042451727a6133622f4cd8d66a84
Djent- <inflames2361@aim.com>
2014-06-12 21:41:48 -0400

    added base rotating square code from LOVE-rotating-squares.git

---
+++ main.lua
@@ -0,0 +1,40 @@
+square = {}
+squares = {}
+
+function love.load()
+	love.graphics.setBackgroundColor(255,255,255)
+	love.graphics.setColor(0,0,0)
+end
+
+function love.update(dt)
+	if #squares == 0 then return end
+	for x = 1, #squares do
+		squares[x].angle = squares[x].angle + math.pi * dt
+	end
+end
+
+function love.draw()
+	if #squares == 0 then return end
+	for x = 1, #squares do
+		love.graphics.draw(squares[x].canvas, squares[x].x, squares[x].y,
+			squares[x].angle, 1, 1, squares[x].w/2, squares[x].h/2)
+	end
+end
+
+function love.mousereleased(x,y)
+	table.insert(squares, square:new(x,y))
+end
+
+function square:new(x,y)
+	local self = {}
+	self.x = x
+	self.y = y
+	self.w = 40
+	self.h = 40
+	self.angle = 0
+	self.canvas = love.graphics.newCanvas(40,40)
+	love.graphics.setCanvas(self.canvas)
+	love.graphics.rectangle("line", 0, 0, 40, 40)
+	love.graphics.setCanvas()
+	return self
+end
